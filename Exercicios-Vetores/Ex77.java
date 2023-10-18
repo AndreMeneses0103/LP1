aa37cc8bf4c85aec405167dacc4817fd717aec84
andremeneses0103 <andremeneses0103@gmail.com>
2023-10-18 10:09:33 -0300

    Adicionando teste de mesa do ex 77 e finalizando todas as atividades.

--- Exercicios-Vetores/Ex77.java
+++ Exercicios-Vetores/Ex77.java
@@ -2,7 +2,9 @@ public class Ex77 {
     public static void main(String[] args) {
         int[] v = {5, 1, 4, 2, 7, 8, 3, 6};
         for (int i = 7; i >= 4; i--) {
+            System.out.println("I = " + i);
             int aux = v[i];
+            System.out.println("AUX -> " + aux);
             v[i] = v[7 - i + 1];
             v[6 - i + 1] = aux;
         }
